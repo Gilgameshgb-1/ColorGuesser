f22acf582e7b5b052ce2e1daeb1f7c0a2b2d5c74
Gilgameshgb-1 <farisbazdar1@gmail.com>
2023-07-26 17:15:59 +0200

    revert + yml

--- mainMenu/main.js
+++ mainMenu/main.js
@@ -1,9 +1,9 @@
 document.getElementById("endless-mode").addEventListener("click", function () {
-  window.location.href = "quizMenu/quiz.html?mode=endless";
+  window.location.href = "../quizMenu/quiz.html?mode=endless";
 });
 
 document
   .getElementById("ten-questions-mode")
   .addEventListener("click", function () {
-    window.location.href = "quizMenu/quiz.html?mode=ten-questions";
+    window.location.href = "../quizMenu/quiz.html?mode=ten-questions";
   });
