756b3b6f2b61326c6db20c4386eeefbe02018496
Gilgameshgb-1 <farisbazdar1@gmail.com>
2023-07-29 15:27:22 +0200

    Landing page update

--- mainMenu/main.js
+++ mainMenu/main.js
@@ -1,9 +1,29 @@
-document.getElementById("endless-mode").addEventListener("click", function () {
-  window.location.href = "../quizMenu/quiz.html?mode=endless";
-});
+// JavaScript to handle the popup functionality
+const playButton = document.getElementById("play-button");
+const modal = document.getElementById("playModal");
+const closeModal = document.getElementById("closeModal");
+const endlessMode = document.getElementById("endlessMode");
+const tenQuestionsMode = document.getElementById("tenQuestions");
 
-document
-  .getElementById("ten-questions-mode")
-  .addEventListener("click", function () {
-    window.location.href = "../quizMenu/quiz.html?mode=ten-questions";
-  });
+// Function to show the modal
+function showModal() {
+  modal.style.display = "block";
+}
+
+// Function to hide the modal
+function hideModal() {
+  modal.style.display = "none";
+}
+
+function redirectEndless() {
+  window.location.href = "../quizMenu/quiz.html?mode=endless";
+}
+
+function redirectTenQuestions() {
+  window.location.href = "../quizMenu/quiz.html?mode=ten-questions";
+}
+
+playButton.addEventListener("click", showModal);
+closeModal.addEventListener("click", hideModal);
+endlessMode.addEventListener("click", redirectEndless);
+tenQuestionsMode.addEventListener("click", redirectTenQuestions);
